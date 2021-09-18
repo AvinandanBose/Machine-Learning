c0dae34fda50f67dbe4a20a19f24d5b86e8be2e9
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2021-09-18 22:19:16 +0530

    Add files via upload

---
+++ PolyMatLaB.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+p = [3,2,1]
+x1 = np.poly1d(p)
+y = np.poly(p)
+print(x1)
+print(y)
+
---
+++ PolyValMatLaB.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+p = [3,2,1,[1,2,3]]
+x = [5,7,9]
+x1 = np.poly1d(p)
+y = np.polyval(p,x)
+print(x1)
+print(y)
